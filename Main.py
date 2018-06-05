0085fe175372e308bb50bbe52ff1d5b14a887b42
wyu19 <wyu711@gmail.com>
2018-06-04 23:55:54 -0700

    created dict class and links to all nba team roster and stats page.

--- Main.py
+++ Main.py
@@ -1,9 +1,8 @@
 import requests
+import pprint
 from bs4 import BeautifulSoup
 from html.parser import HTMLParser
 
-# maybe make it so we can see how much pts, asst, and rebounds each player gets on avg for every team.
-# then write a script to automatically put in all the teams in.
 class TagStripper(HTMLParser):
 
     def __init__(self):
@@ -17,6 +16,12 @@ class TagStripper(HTMLParser):
 
     def get_data(self):
         return ''.join(self.fed)
+
+
+class NestedDict(dict):
+    def __missing__(self, key):
+        value = self[key] =  type(self)()
+        return value
 
 
 def strip_tags(html):
@@ -57,32 +62,44 @@ def team_table(roster):
 
 
 url_dict = {}
-cont = True
-while cont:
-    url = input("Enter a url to see if they are scrapable: ")
-    req = connect(url, name="test_name", email="test_mail")
-    html = req.text
-    soup = BeautifulSoup(html, "html.parser")
-    title = soup.title.string
-    print("\n"+ title + "\n")
-    roster_info = soup.find_all('td')
-    team = strip_tags(str(roster_info)).strip('] [')
-    team = team.split(', ')
-    team_table(team)
+team_initials = ['gsw', 'bos', 'atl', 'bkn', 'cha', 'chi', 'cle', 'dal', 'den', 'det', 'hou',
+                 'ind', 'lac', 'mem', 'mia', 'mil', 'nop', 'nyk', 'okc', 'orl', 'phi', 'phx',
+                 'por', 'sac', 'sas', 'tor', 'uta', 'was'
+                 ]
+roster_link = 'http://www.espn.com/nba/team/roster/_/name/'
+stats_link = 'http://www.espn.com/nba/team/stats/_/name/'
+
+for init in team_initials:
+    roster_url = roster_link+init
+    stat_url = stats_link+init
+    roster_req = connect(roster_url, name="test", email="test_mail")
+    stat_req = connect(stat_url, name="test", email="test_mail")
 
 
-
-    #http://www.espn.com/nba/team/roster/_/name/gs/index  scrape this!
-    url_dict.update({url: req})
-
-    if req.status_code != 200:
-        if req.status_code == 404:
-            print("robots.txt not found for {}").format(str(url))
-        else:
-            print("There has been a error with scraping this site {}").format(str(url))
-
-    add_more = input("Do you want to continue? Enter yes or no: ")
-    if add_more == 'no':
-        break;
-
-print(url_dict)
+#
+# cont = True
+# while cont:
+#     url = input("Enter a url to see if they are scrapable: ")
+#     req = connect(url, name="test_name", email="test_mail")
+#     html = req.text
+#     soup = BeautifulSoup(html, "html.parser")
+#     title = soup.title.string
+#     print("\n" + title + "\n")
+#     roster_info = soup.find_all('td')
+#     team = strip_tags(str(roster_info)).strip('] [')
+#     team = team.split(', ')
+#     team_table(team)
+#
+#     url_dict.update({url: req})
+#
+#     if req.status_code != 200:
+#         if req.status_code == 404:
+#             print("robots.txt not found for {}").format(str(url))
+#         else:
+#             print("There has been a error with scraping this site {}").format(str(url))
+#
+#     add_more = input("Do you want to continue? Enter yes or no: ")
+#     if add_more == 'no':
+#         break;
+#
+# print(url_dict)
