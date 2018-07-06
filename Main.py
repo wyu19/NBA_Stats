b23ef4ce174c5c1f72d582507a39b17477601566
wyu19 <wyu711@gmail.com>
2018-07-06 16:43:59 -0700

    began using dash to create graphs

--- Main.py
+++ Main.py
@@ -6,6 +6,7 @@ import dash_html_components as html
 from bs4 import BeautifulSoup
 from html.parser import HTMLParser
 
+
 class TagStripper(HTMLParser):
 
     def __init__(self):
@@ -19,12 +20,6 @@ class TagStripper(HTMLParser):
 
     def get_data(self):
         return ''.join(self.fed)
-
-
-class Vividict(dict):
-    def __missing__(self, key):
-        value = self[key] = type(self)()
-        return value
 
 
 def strip_tags(html):
@@ -64,24 +59,12 @@ def team_table(roster):
             player_info = "             "
 
 
-
-
 def extract_info(data, skip, *break_string):
     info_list = data[0::skip]
     if 'Totals' in info_list:
         index = info_list.index('Totals')
         info_list = info_list[:index]
     return info_list
-
-# def fill_stats(matr, team_dict, stats_list):  #problem is that stats dict will have the repetition of names
-#     team_dict
-#
-#
-#   # for p in team_dict:
-#   #     for i,s in zip(stats_matrix, stats_list):
-#   #         team_dict[p]
-#   #
-#remove all the unnecessary stats and create a new list with all the salaries in it.
 
 
 def sort_allstats(stats, stat_indices):
@@ -107,29 +90,42 @@ def fill_dict(t_dict, w_stats, stats_symbols, salary):
     for p in t_dict:
         for s, i in zip(w_stats, stats_symbols):
             if i == "Salary":
-                if p not in salary:
+                if p not in salary:              #if the player from previous season is no longer in the current roster just give them a 0
                     t_dict[p][i] = 0
                 else:
                     t_dict[p][i] = salary[p]
             else:
-                t_dict[p][i] = s            #create a case to handle exception(player no longer on roster and does not have a salary)
+                t_dict[p][i] = s
         w_stats = w_stats[7:]
 
+
+def zero_salaries(t_dict):  #makes all the salaries that are '\xa0' = 0
+    for p in t_dict:
+        if t_dict[p]['Salary'] == '\xa0':
+           t_dict[p]['Salary'] = 0
+
 #make a player:salary dictionary so i can call it to fill in the salary part.
+
+
 def create_salary_dict(roster_info):
-    player = [p for i, p in enumerate(roster_info) if i%8 == 0]
-    salary = [s for i, s in enumerate(roster_info[6:]) if i%8 == 0]  #this isnt right
-    print(player)
-    print(salary)
+    player = [p for i, p in enumerate(roster_info) if i% 8 == 0]
+    salary = [s for i, s in enumerate(roster_info[6:]) if i% 8 == 0]
     sal_dict = dict(zip(player, salary))
     return sal_dict
 
+
+def print_team(team_dict):
+    for i in team_dict:
+        for p in team_dict[i]:
+            print("{} {}".format(p, team_dict[i][p]))
+
 #team_dict[teams][player][stats]
 #reused variables
+
 url_dict = {}
 team_initials = ['gs', 'bos', 'atl', 'bkn', 'cha', 'chi', 'cle', 'dal', 'den', 'det', 'hou',
-                  'ind', 'lac', 'mem', 'mia', 'mil', 'nop', 'nyk', 'okc', 'orl', 'phi', 'phx',
-                 'por', 'sac', 'sas', 'tor', 'uta', 'was'
+                  'ind', 'lac', 'mem', 'mia', 'mil', 'no', 'nyk', 'okc', 'orl', 'phi', 'phx',
+                 'por', 'sac', 'sas', 'tor', 'utah', 'was'
                 ]
 
 roster_link = 'http://www.espn.com/nba/team/roster/_/name/'
@@ -140,6 +136,7 @@ season_type = "/seasontype/2"
 
 #add salary to the end of all the stats list.
 #accesses all the nba roster links and infos
+
 for t, init in enumerate(team_initials, 0):
     roster_url = roster_link+init
     stat_url = stats_link+init+season_type
@@ -159,23 +156,16 @@ for t, init in enumerate(team_initials, 0):
     roster = roster.split(', ')
 
     players = extract_info(all_stats[16:], 16, 'Totals')              #sort out the players and salaries from the roster link
-    salaries = extract_info(roster[16:], 8)
-
     player_salaries = create_salary_dict(roster[10:])        #need to cut off the extra stuff from after players
-    print(player_salaries)
-    print(players)
+    salaries = list(player_salaries.values())
 
     players_dict = {p: {s: 0 for s in stats_list} for p in players}  #created the dictionary with all the players from one team
-    print(players_dict)
     team_dict = {team_initials[t]: {p: {s: 0 for s in stats_list} for p in players}}
-    print(team_dict)
     stats_indices = [2, 4, 5, 8, 9, 10, 11]
     wanted_stats = sort_allstats(all_stats[16:], stats_indices)
-    print(wanted_stats)
     fill_dict(team_dict[team_initials[t]], wanted_stats, stats_list, player_salaries)
-    print(team_dict)
-# i just need to fill in the rest of the stats
-
+    zero_salaries(team_dict[team_initials[t]])
+    print_team(team_dict)
 
     url_dict.update({roster_url: roster_req})
     url_dict.update({stat_url: roster_req})
@@ -191,3 +181,17 @@ for t, init in enumerate(team_initials, 0):
         break;
 
 print(url_dict)
+
+# Graph code ----------------------------------------------------------------
+
+# app = dash.Dash()
+# app.layout = html.Div(children=[html.H1('NBA Statistics'), dcc.Graph(id='all_stats',
+#                                                                      figure ={
+#                                                                          'data': [{'x': list(player_salaries.keys()),
+#                                                                                    'y': list(player_salaries.values()),
+#                                                                                   'type': 'line', 'name': 'boats'}]
+#                                                                           })
+#                                                                           ])
+#
+# if __name__ == '__main__':
+#     app.run_server(debug=True)
